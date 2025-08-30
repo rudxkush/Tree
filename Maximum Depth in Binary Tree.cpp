012d22e4465eb2434cd154dc6d4eb4a93240a634
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-08-30 11:53:26 +0530

    Create Maximum Depth in Binary Tree.cpp

---
+++ Maximum Depth in Binary Tree.cpp
@@ -0,0 +1,10 @@
+int rec(TreeNode* root) {
+	if(root == NULL) return 0;
+	int left = rec(root->left);
+  int right = rec(root->right);
+	return 1 + max(left, right);
+} 
+
+int maxHeight(TreeNode* root) {
+	return rec(root);
+}
